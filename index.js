5df4d126e60313192770ca11357aee76b1e55a30
Sajithrajan03 <saisajith03@gmail.com>
2024-02-22 00:45:21 +0530

    Setted Up

---
+++ index.js
@@ -0,0 +1,23 @@
+import mysql from 'mysql2/promise';
+
+// Create the connection to database
+const connection = await mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'test',
+});
+
+// A simple SELECT query
+try {
+  const [results, fields] = await connection.query(
+    'SELECT * FROM sample ;'
+  );
+
+  console.log(results); // results contains rows returned by server
+  console.log(fields); // fields contains extra meta data about results, if available
+} catch (err) {
+  console.log(err);
+}
+
+// Using placeholders
+ 
