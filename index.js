1e4fb2df9d9e562ac1f508c8634e10fd715291fe
Sajithrajan03 <saisajith03@gmail.com>
2024-02-22 00:46:39 +0530

    db changed

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import mysql from 'mysql2/promise';
 const connection = await mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  database: 'test',
+  database: 'TripEstimator',
 });
 
 // A simple SELECT query
