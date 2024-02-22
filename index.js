0dfb30b0df5f3517043ecc2b87080d919cd997eb
Sajithrajan03 <saisajith03@gmail.com>
2024-02-22 12:13:06 +0530

    await

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 const mysql = require('mysql2/promise');
 
-// Create the connection to database
-const connection = await mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'TripEstimator',
-});
+async function fetchData() {
+  try {
+    // Create the connection to database
+    const connection = await mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      database: 'TripEstimator',
+    });
 
-// A simple SELECT query
-try {
-  const [results, fields] = await connection.query(
-    'SELECT * FROM sample ;'
-  );
+    // A simple SELECT query
+    const [results, fields] = await connection.query('SELECT * FROM sample;');
 
-  console.log(results); // results contains rows returned by server
-  console.log(fields); // fields contains extra meta data about results, if available
-} catch (err) {
-  console.log(err);
+    console.log(results); // results contains rows returned by server
+    console.log(fields); // fields contains extra meta data about results, if available
+
+    // Close the connection
+    await connection.end();
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-// Using placeholders
- 
+// Call the fetchData function
+fetchData();
